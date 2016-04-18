5253a44b14aa31d1a2ab76d6a69affd36fbc2901
mwcz <mwc@clayto.com>
2016-04-18 10:04:11 -0400

    don't pause game when unfocused

--- client/src/states/Boot.js
+++ client/src/states/Boot.js
@@ -11,6 +11,9 @@ class BootState extends Phaser.State {
         // Just remember to remove it/comment it out when you release, since this plugin eats CPU too. Also see index.html
         // this.game.add.plugin(Phaser.Plugin.Debug); // <-- Phaser Debug Plugin
 
+        //  Phaser will automatically pause if the browser tab the game is in
+        //  loses focus. You can disable that here:
+        this.stage.disableVisibilityChange = true;
 
         // Uncomment this if you want to use the phaser tiled plugin. Also see index.html
         // this.game.add.plugin(Phaser.Plugin.Tiled); // <-- Phaser Tiled Plugin
