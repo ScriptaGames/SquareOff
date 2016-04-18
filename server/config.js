a8d1f3d9e1ac3f00bbe6398b4a1811548739cdd8
mwcz <mwc@clayto.com>
2016-04-18 14:58:14 -0400

    increase max speed

--- server/config.js
+++ server/config.js
@@ -16,8 +16,8 @@ module.exports = {
     DISC: {
         DIAMETER: 1.95, // as a percentage of block height/width
         BOUNCE_SPEEDUP: 0.55, // add this amount to disc speed after each bounce
-        INITIAL_SPEED: 2.00,
-        MAX_SPEED: 20.00,
+        INITIAL_SPEED: 30.00,
+        MAX_SPEED: 26.00,
         MOVE_DELAY: 750, // delay before moving; in ms
     },
     TICK_FAST_INTERVAL: 50,
